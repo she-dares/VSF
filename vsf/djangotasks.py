20b87774e64a4ddd0321b75104c024ad1452fe15
Lakshmi.Umarale <lakshmi.umarale@gmail.com>
2019-12-13 20:15:35 -0500

    fix:formatting and some minor fixes

--- vsf/djangotasks.py
+++ vsf/djangotasks.py
@@ -4,14 +4,17 @@ from luigi import ExternalTask, Parameter, Task, BoolParameter
 from utils.luigi.dask import *
 from utils.luigi.task import Requires, Requirement, TargetOutput
 
+
 class LineDimVer(ExternalTask):
     S3_ROOT = "s3://groupprojectcscie29/LineDimension/"
     """
     This class verifies that the target exists for the processing. This task loads the Line Dimension data.
     """
+
     def output(self):
         target = CSVTarget(self.S3_ROOT, flag=False, glob="*.csv")
         return target
+
 
 class LineDimLoad(Task):
     LOCAL_ROOT = "./data/linedim/"
@@ -25,15 +28,17 @@ class LineDimLoad(Task):
     output = TargetOutput(target_class=ParquetTarget, file_pattern=LOCAL_ROOT, ext="")
 
     def run(self):
-        dtype_dic = {"MDN": "object", "DEVICE_GROUPING": "object", "SALES_CHANNEL": "object"}
-        dsk = self.input()["line_dim"].read_dask(
-             dtype=dtype_dic,
-        )
+        dtype_dic = {
+            "MDN": "object",
+            "DEVICE_GROUPING": "object",
+            "SALES_CHANNEL": "object",
+        }
+        dsk = self.input()["line_dim"].read_dask(dtype=dtype_dic)
 
         if self.subset:
             dsk = dsk.get_partition(0)
 
-        self.output().write_dask(dsk,compression="gzip", compute=True)
+        self.output().write_dask(dsk, compression="gzip", compute=True)
 
 
 class AccountDimVer(ExternalTask):
@@ -41,9 +46,11 @@ class AccountDimVer(ExternalTask):
     """
     This class verifies that the target exists for the processing. This task loads the Account Dimension data.
     """
+
     def output(self):
         target = CSVTarget(self.S3_ROOT, flag=False, glob="*.csv")
         return target
+
 
 class AccountDimLoad(Task):
     LOCAL_ROOT = "./data/accountdim/"
@@ -57,15 +64,17 @@ class AccountDimLoad(Task):
     output = TargetOutput(target_class=ParquetTarget, file_pattern=LOCAL_ROOT, ext="")
 
     def run(self):
-        dtype_dic = {"CUST_ACCT": "object", "SEGMENT_NAME": "object", "SVC_PLAN": "object"}
-        dsk = self.input()["account_dim"].read_dask(
-             dtype=dtype_dic,
-        )
+        dtype_dic = {
+            "CUST_ACCT": "object",
+            "SEGMENT_NAME": "object",
+            "SVC_PLAN": "object",
+        }
+        dsk = self.input()["account_dim"].read_dask(dtype=dtype_dic)
 
         if self.subset:
             dsk = dsk.get_partition(0)
 
-        self.output().write_dask(dsk,compression="gzip", compute=True)
+        self.output().write_dask(dsk, compression="gzip", compute=True)
 
 
 class LimitFactVer(ExternalTask):
@@ -73,9 +82,11 @@ class LimitFactVer(ExternalTask):
     """
     This class verifies that the target exists for the processing. This task loads the Limit Facts data.
     """
+
     def output(self):
         target = CSVTarget(self.S3_ROOT, flag=False, glob="*.csv")
         return target
+
 
 class LimitFactLoad(Task):
     LOCAL_ROOT = "./data/limitfact/"
@@ -89,12 +100,15 @@ class LimitFactLoad(Task):
     output = TargetOutput(target_class=ParquetTarget, file_pattern=LOCAL_ROOT, ext="")
 
     def run(self):
-        dtype_dic = {"MTN": "object", "CUST_ACCT": "object", "LIMITING_DT":"object", "LIMIT_TYPE": "object"}
-        dsk = self.input()["limit_fact"].read_dask(
-             dtype=dtype_dic,
-        )
+        dtype_dic = {
+            "MTN": "object",
+            "CUST_ACCT": "object",
+            "LIMITING_DT": "object",
+            "LIMIT_TYPE": "object",
+        }
+        dsk = self.input()["limit_fact"].read_dask(dtype=dtype_dic)
 
         if self.subset:
             dsk = dsk.get_partition(0)
 
-        self.output().write_dask(dsk,compression="gzip", compute=True)
+        self.output().write_dask(dsk, compression="gzip", compute=True)
