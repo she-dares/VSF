231f21d9f7399e5f4a0a300002481afd5220eddb
Lakshmi.Umarale <lakshmi.umarale@gmail.com>
2019-12-13 20:11:51 -0500

    feat:implemented all the test cases for the django model/views/serializer

--- test_pset.py
+++ test_pset.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import datetime
+from django.test import TestCase as DJTest
 from unittest import TestCase
+from vsf_eventapp.models import LineDim, ActivityFact, LimitFact, AccountDim
 
 
-class MyTest(TestCase):
-    def test_blah(self):
+class APIViewTests(DJTest):
+    def setUp(self):
 
-        assert True
+        dates = [datetime.date(y, 1, 1) for y in range(2010, 2019)]
+        mtn_id = 1000
+        act_id = 2000
 
+        activityfactobj = []
+        limitfactobj = []
+        for d in dates:
+            activityfactobj.append(
+                ActivityFact.objects.get_or_create(
+                    MTN=LineDim.objects.get_or_create(MTN=mtn_id)[0], EVENT_DT=d
+                )
+            )
+            limitfactobj.append(
+                LimitFact.objects.get_or_create(
+                    MTN=LineDim.objects.get_or_create(MTN=mtn_id)[0],
+                    LIMIT_DT=d,
+                    LIMIT_TYPE="USG",
+                    Cust_Acct=AccountDim.objects.get_or_create(Cust_Acct=act_id)[0],
+                )
+            )
+            mtn_id = mtn_id + 1
+            act_id = act_id + 1
+
+    def test_activity_fact(self):
+        self.assertEqual(LineDim.objects.count(), 9)
+        self.assertEqual(ActivityFact.objects.count(), 9)
+        self.assertEqual(AccountDim.objects.count(), 9)
