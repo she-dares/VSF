be4ed6774c088d8df07c893312fffa8ce00dd60f
Lakshmi.Umarale <lakshmi.umarale@gmail.com>
2019-12-13 20:16:23 -0500

    fix:removed some code

--- vsf_eventapp/serializers.py
+++ vsf_eventapp/serializers.py
@@ -4,15 +4,3 @@ from .models import ActivityFact
 # from django.db import models
 from rest_framework.serializers import FloatField, IntegerField
 
-class ByYearSerializer(Serializer):
-    # Return the *averages*, not the sum!
-    year = IntegerField(default=0)
-    tot_count = IntegerField(default=0)
-    stars_avg = FloatField(default=0.0)
-    useful_avg = FloatField(default=0.0)
-    funny_avg = FloatField(default=0.0)
-    cool_avg = FloatField(default=0.0)
-
-    class Meta:
-        # model = ActivityFact
-        fields = "__all__"
