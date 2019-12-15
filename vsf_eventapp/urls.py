0dd2783652486b79d0f1e9f48207e25d52b5c132
Lakshmi.Umarale <lakshmi.umarale@gmail.com>
2019-12-15 16:37:36 -0500

    fix:added urls for byactivity

--- vsf_eventapp/urls.py
+++ vsf_eventapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
-from .views import ByLimit, ActivityViewSet, render_aggregation
+from .views import ByLimit, ActivityViewSet, render_aggregation, ByActivity
 
 router = DefaultRouter()
 
@@ -10,11 +10,13 @@ router = DefaultRouter()
 
 router.register("activity", ActivityViewSet, basename="activity")
 router.register("by_limit", ByLimit, basename="by_limit")
+router.register("by_device", ByActivity, basename="by_device")
 
 schema_view = get_schema_view(title="VSF API")
 
 urlpatterns = [
     path("api/", include(router.urls)),
     path("MTN_BY_LIMIT/", ByLimit, name="aggregation"),
+    path("MTN_BY_DEVICE/", ByActivity, name="aggregation"),
     path("", render_aggregation, name="aggregation"),
 ]
